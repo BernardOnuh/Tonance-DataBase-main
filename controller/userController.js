71ca59c89b1b95f1f767baf4e825cec5d6b5d475
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-09-07 10:35:52 +0100

    Tas

--- controller/userController.js
+++ controller/userController.js
@@ -179,3 +179,18 @@ exports.completeTask = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getCompletedTasks = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate('tasksCompleted');
+    
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user.tasksCompleted);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
