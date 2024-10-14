f7a975d0556bef4887c092ab2d856e50b262ae7e
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-10-14 18:55:43 +0100

    Update

--- controller/dailyPointController.js
+++ controller/dailyPointController.js
@@ -1,5 +1,5 @@
 const DailyPoint = require('../models/DailyPoint');
-const User = require('../models/User');
+const { User } = require('../models/User');
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
